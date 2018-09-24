017a20f536bfa23c0795af1997c4580006af3d7e
Cyndy Koobs <cyndy.koobs@att.net>
2018-09-24 16:41:50 -0500

    add randomization of image population

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,10 +1,7 @@
 // console.log($);
 
 $(() => {
-  const $flexContainer =  $( '.flex-container' );
-  console.log($flexContainer);
-
-  $flexContainer.on('click', () => {
+  $( '.flex-container' ).on('click', () => {
     console.log('clicked image');
   });
 
@@ -26,28 +23,56 @@ let selectedImages = [];
 let images = ['./img/tawny_owl.jpg',
                       './img/ural_owl.jpg',
                       './img/snowy_owl.jpg',
+                      './img/owl_burrowing.jpg',
+                      './img/tawny_owl.jpg',
+                      './img/ural_owl.jpg',
+                      './img/snowy_owl.jpg',
                       './img/owl_burrowing.jpg'];
 
 // retains order of image ids in the display createImageArray
 let imageOrder = [];
 
-const createImages = () => {
+// randomize the order in which the images are displayed
+const createBoard = () => {
 
 };
 
+const createImageOrder = () => {
+  let index = 0;
+  let filled = false;
+
+  while (!filled) {
+    // for (let i = 0; i < images.length; i++) {
+    index = Math.floor(Math.random() * (images.length - 0 + 1));
+    // can't have the same number twice...
+    if (imageOrder.indexOf(index) === -1) {
+      imageOrder.push(index);
+    }
+
+    // all images populated??
+    if (imageOrder.length === images.length) {
+      filled = true;
+    }
+  }
+  console.log(imageOrder);
+}
+
 const addImages = () => {
+  createImageOrder();
+
   //select container to add images
   const $flexContainer = $( '.flex-container' );
   // const $gameBoard = $('#game-board');
 
   // add to $flexContainer
-  for (let i = 0; i < images.length; i++) {
+  for (let i = 0; i < imageOrder.length; i++) {
     //create an image and add a unique id
-    let imageid = `image${i}`;
+    let arrayIdx = imageOrder[i];
+    let imageid = `image${arrayIdx}`;
     // console.log('id: ' + imageid);
 
     const $image = $( '<img>' );
-    $image.attr("src", images[i]);
+    $image.attr("src", images[arrayIdx]);
     $image.attr("id", imageid);
     $image.addClass('image');
     //add an event listner/handler that toggles the class selected for the card that is clicked
