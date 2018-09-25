1582ff64802c1739a5b5f5911198471450e61c60
Cyndy Koobs <cyndy.koobs@att.net>
2018-09-25 12:57:38 -0500

    add image 'back'; additional styling, buttons/event handlers, Raleway font

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,18 +1,25 @@
-// console.log($);
 
 $(() => {
-  $( '.flex-container' ).on('click', () => {
-    console.log('clicked image');
+  // $( '.flex-container' ).on('click', () => {
+  //   console.log('clicked image');
+  // });
+  $('#start').on('click', () => {
+
   });
 
-  addImages();
+  $('#reset').on('click', resetImages);
+
+  createImageArray();
 })
+
+// state of game; gets set when 'game' begins
+let state = 0
 
 // container for image objects
 let imageObjects = [];
 
 // image object; contains properties of images added to game board
-let image = {
+const imageObject = {
   front: '',
   back: '',
   id: ''
@@ -20,35 +27,36 @@ let image = {
 
 let selectedImages = [];
 
+let imageBack = './img/back.jpg';
+
 let images = ['./img/tawny_owl.jpg',
-                      './img/ural_owl.jpg',
-                      './img/snowy_owl.jpg',
-                      './img/owl_burrowing.jpg',
-                      './img/tawny_owl.jpg',
-                      './img/ural_owl.jpg',
-                      './img/snowy_owl.jpg',
-                      './img/owl_burrowing.jpg'];
+                './img/ural_owl.jpg',
+                './img/snowy_owl.jpg',
+                './img/owl_burrowing.jpg',
+                './img/tawny_owl.jpg',
+                './img/ural_owl.jpg',
+                './img/snowy_owl.jpg',
+                './img/owl_burrowing.jpg'];
 
 // retains order of image ids in the display createImageArray
 let imageOrder = [];
 
-// randomize the order in which the images are displayed
 const createBoard = () => {
 
 };
 
+// randomize the order in which the images are displayed
 const createImageOrder = () => {
   let index = 0;
   let filled = false;
 
   while (!filled) {
-    // for (let i = 0; i < images.length; i++) {
-    index = Math.floor(Math.random() * (images.length - 0 + 1));
+    index = Math.floor(Math.random() * images.length);
+
     // can't have the same number twice...
     if (imageOrder.indexOf(index) === -1) {
       imageOrder.push(index);
     }
-
     // all images populated??
     if (imageOrder.length === images.length) {
       filled = true;
@@ -57,39 +65,76 @@ const createImageOrder = () => {
   console.log(imageOrder);
 }
 
-const addImages = () => {
+const createImageArray = () => {
   createImageOrder();
 
   //select container to add images
   const $flexContainer = $( '.flex-container' );
   // const $gameBoard = $('#game-board');
 
+  let arrayIdx = 0;
+  let imageid  = 0;
+  let imageToDisplay = '';
+
   // add to $flexContainer
   for (let i = 0; i < imageOrder.length; i++) {
     //create an image and add a unique id
-    let arrayIdx = imageOrder[i];
-    let imageid = `image${arrayIdx}`;
-    // console.log('id: ' + imageid);
+    arrayIdx = imageOrder[i];
+    // let imageid = `image${arrayIdx}`;
+    imageid = arrayIdx;
 
-    const $image = $( '<img>' );
-    $image.attr("src", images[arrayIdx]);
-    $image.attr("id", imageid);
-    $image.addClass('image');
+    // based on game state select front or background
+    imageToDisplay = (state === 0) ? imageBack : images[arrayIdx];
+    const $image = $( '<img>' )
+      .attr("src", imageToDisplay)
+      .attr("id", imageid)
+      .addClass('image');
+
+    let imageObj = {};
+    imageObj.front = images[arrayIdx];
+    imageObj.back = imageBack;
+    imageObj.id = arrayIdx;
+
+    imageObjects.push($image);
+
+
     //add an event listner/handler that toggles the class selected for the card that is clicked
+    $image.one('click', flipImage) ;
 
+    // $(image).one('click', () => {
+    //   console.log('image ' + $(image));
+    // })
     $flexContainer.append( $image );
-    // $gameBoard.append( $image );
   }
 }
 
-const createImageArray = () => {
-
+const resetImages = () => {
+  selectedImages = [];
 }
 
-const flipImage = () => {
+const flipImage = (event) => {
+  let imageId = $(event.currentTarget).attr('id');
+  // get the image from the array
+  let image = images[imageId];
+
+  // keep track of what images are selected
+  selectedImages.push(image);
+  console.log(selectedImages);
+
+  // enough selections to check for a match?
+  if (selectedImages.length === 2) {
+    checkForMatch();
+  }
 
 }
 
 const checkForMatch = () => {
+  if (selectedImages[0] === selectedImages[1]) {
+    alert('matched');
+  } else {
+    // flip them over; reset selected Images
+    selectedImages = [];
+  }
+
 
 }
