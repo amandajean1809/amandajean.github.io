fd9c1735dfad23fffc86b0d55fc04e8e846d5ab6
Cyndy Koobs <cyndy.koobs@att.net>
2018-09-26 14:20:24 -0500

    add players and game information

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,23 +1,26 @@
 
 $(() => {
-  $('#start').on('click', () => {
+  $('#start').on('click', startGame);
 
-  });
-
-  $('#reset').on('click', resetImages);
+  $('#reset').on('click', resetGame);
 
   createImageArray();
 })
 
+const game = {
+  currentPlayer: 0,      // 0 or 1 (player1 or player2)
+  matches1: 0,
+  matches2: 0,
+  chances1: 0,
+  chances2: 0,
+  player1: '',
+  player2: ''
+};
+
+let currentGame = {};
+
 // container for image objects
 let imageObjects = [];
-
-// image object; contains properties of images added to game board
-const imageObject = {
-  front: '',
-  back: '',
-  id: ''
-};
 
 let selectedImages = [];
 
@@ -34,10 +37,6 @@ let images = ['./img/tawny_owl.jpg',
 
 // retains order of image ids in the display createImageArray
 let imageOrder = [];
-
-const createBoard = () => {
-
-};
 
 // randomize the order in which the images are displayed
 const createImageOrder = () => {
@@ -63,9 +62,7 @@ const createImageArray = () => {
 
   //select container to add images
   const $flexContainer = $( '.flex-container' );
-  // const $gameBoard = $('#game-board');
 
-  let arrayIdx = 0;
   let imageid  = 0;
   let imageToDisplay = '';
 
@@ -73,8 +70,6 @@ const createImageArray = () => {
   for (let i = 0; i < imageOrder.length; i++) {
     //create an image and add a unique id
     imageid = imageOrder[i];
-    // let imageid = `image${arrayIdx}`;
-    // imageid = arrayIdx;
 
     // create 'container div'
     const $section = $('<section>')
@@ -93,64 +88,35 @@ const createImageArray = () => {
       .css('background-image', 'url(' + images[imageid] + ')')
       .addClass('back');
 
-    $section.append($cardDiv);
-    $cardDiv.append($front);
-    $cardDiv.append($back);
-    // create inner div
-    // const $cardDiv = $('<div>')
-    //   .addClass('card')
-    //   .attr("id", imageid)
-    //   .on('click', flipImage);
-    //
-    // // create 'front' div
-    // const $frontDiv = $('<div>')
-    //   .addClass('front');
-    //
-    // const $front = $( '<img>' )
-    //   .attr("src", imageBack);
-    //
-    // // create 'back' div
-    // const $backDiv = $('<div>')
-    //   .addClass('back');
-    //
-    // const $back = $( '<img>' )
-    //   .attr("src", images[arrayIdx]);
-    //
-    // $section.append($cardDiv);
-    // $cardDiv.append($frontDiv);
-    // $cardDiv.append($backDiv);
-    // $backDiv.append($back);
-    // $frontDiv.append($front);
+    $section.append( $cardDiv );
+    $cardDiv.append( $front );
+    $cardDiv.append( $back );
 
     $flexContainer.append( $section );
   }
 }
 
-const resetImages = () => {
+const resetGame = () => {
   selectedImages = [];
+  currentGame = {};
+
+
 }
 
 const flipImage = (event) => {
-  let $target = $(event.currentTarget);
-  let $cardChildren = $( event.currentTarget).children();
-  let $front = $( event.currentTarget).children('div:first');
-  // let $card = ($('.card')).getParent();
-  // console.log($card);
+  let $target = $( event.currentTarget );
+  // let $cardChildren = $( event.currentTarget ).children();
+  let $card = $( event.currentTarget ).children('div:first');
   $target.toggleClass('flipped');
-  // $target.attr("backface-visibility", "hidden");
-  // console.log($(event.target));
-  // console.log($(event.currentTarget));
-  // // get the image from the array
-  // let image = images[imageId];
-  //
+
   // // keep track of what images are selected
-  // selectedImages.push(image);
-  // console.log(selectedImages);
-  //
-  // // enough selections to check for a match?
-  // if (selectedImages.length === 2) {
-  //   checkForMatch();
-  // }
+  selectedImages.push( $card );
+  console.log( selectedImages );
+
+  // enough selections to check for a match?
+  if (selectedImages.length === 2) {
+    checkForMatch();
+  }
   event.preventDefault();
 }
 
@@ -161,42 +127,26 @@ const checkForMatch = () => {
     // flip them over; reset selected Images
     selectedImages = [];
   }
+}
 
-  // create 'container div'
-  // const $flipBoxDiv = $('<div>')
-  //   .addClass('flip-box');
-  //
-  // // create inner div
-  // const $flipBoxInnerDiv = $('<div>')
-  //   .addClass('flip-box-inner')
-  //   .on('click', flipImage);
-  //
-  // // $flipBoxDiv.append($flipBoxInnerDiv);
-  //
-  // // create 'front' div
-  // const $flipBoxFrontDiv = $('<div>')
-  //   .addClass('flip-box-front');
-  //
-  // // $flipBoxInnerDiv.append($flipBoxFrontDiv);
-  //
-  // const $imagefront = $( '<img>' )
-  //   .attr("src", images[arrayIdx])
-  //   .attr("id", imageid);
-  //
-  // // $flipBoxFrontDiv.append($imagefront);
-  //
-  // // create 'back' div
-  // const $flipBoxBackDiv = $('<div>')
-  //   .addClass('flip-box-back');
-  //
-  // const $imageback = $( '<img>' )
-  //   .attr("src", imageBack)
-  //   .attr("id", imageid);
-  //
-  // $flipBoxFrontDiv.append($imagefront);
-  // $flipBoxBackDiv.append($imageback);
-  // $flipBoxInnerDiv.append($flipBoxFrontDiv);
-  // $flipBoxInnerDiv.append($flipBoxBackDiv);
-  // $flipBoxDiv.append($flipBoxInnerDiv);
+const startGame = () => {
+  // get player input; save in game object
+  currentGame.player1 = $( '#player1' ).val();
+  currentGame.player2 = $( '#player2' ).val();
 
+  // check to make sure both input fields are populated
+  if ((currentGame.player1 === '') || (currentGame.player2 === '')) {
+    alert('Both players need to enter a name before the game can begin.\nPlease enter both player\'s names and restart the game');
+  } else {
+    let playerToStart = Math.floor(Math.random() * 1);
+
+    // update the game details
+    currentGame.currrentPlayer = playerToStart;
+
+    // TODO: change this to message on the page...
+    let starter = (playerToStart === 0) : currentGame.player1 : currentGame.player2;
+
+    alert(`${starter} starts the game. First player with 3 matches and less than 3 "misses" wins. Good Luck!`);
+    console.log(starter);
+  }
 }
